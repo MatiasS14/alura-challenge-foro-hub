82213e8c8c3734f42785c17529f69ee530702f3e
MatiasS14 <matias.solanes14@gmail.com>
2025-07-21 16:05:28 -0300

    Proyecto inicial del challenge

---
+++ Proyecto-foro-hub/src/main/java/challenge/foro/hub/api/Proyecto/foro/hub/ProyectoForoHubApplication.java
@@ -0,0 +1,13 @@
+package challenge.foro.hub.api.Proyecto.foro.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoForoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoForoHubApplication.class, args);
+	}
+
+}
