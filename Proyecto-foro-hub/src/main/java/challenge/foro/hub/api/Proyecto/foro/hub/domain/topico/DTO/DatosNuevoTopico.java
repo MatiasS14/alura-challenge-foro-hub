d8246d178941c73bbd2d05e3df6cbad33bf38d6a
MatiasS14 <matias.solanes14@gmail.com>
2025-08-09 18:01:31 -0300

    imports innecesarios borrados

--- Proyecto-foro-hub/src/main/java/challenge/foro/hub/api/Proyecto/foro/hub/domain/topico/DTO/DatosNuevoTopico.java
+++ Proyecto-foro-hub/src/main/java/challenge/foro/hub/api/Proyecto/foro/hub/domain/topico/DTO/DatosNuevoTopico.java
@@ -1,10 +1,8 @@
 package challenge.foro.hub.api.Proyecto.foro.hub.domain.topico.DTO;
 
 import challenge.foro.hub.api.Proyecto.foro.hub.domain.curso.DTO.DatosCurso;
-import challenge.foro.hub.api.Proyecto.foro.hub.domain.topico.Status;
 import challenge.foro.hub.api.Proyecto.foro.hub.domain.usuario.DTO.DatosUsuario;
 
-import java.time.LocalDateTime;
 
 public record DatosNuevoTopico(
         String titulo,
