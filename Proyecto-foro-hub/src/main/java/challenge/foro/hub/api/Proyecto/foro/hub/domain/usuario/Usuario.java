48e3c5c63faecf41397da077c68b1a79eabb62f7
MatiasS14 <matias.solanes14@gmail.com>
2025-08-09 17:59:26 -0300

    Configuraciones de seguridad en usuario

--- Proyecto-foro-hub/src/main/java/challenge/foro/hub/api/Proyecto/foro/hub/domain/usuario/Usuario.java
+++ Proyecto-foro-hub/src/main/java/challenge/foro/hub/api/Proyecto/foro/hub/domain/usuario/Usuario.java
@@ -1,13 +1,17 @@
 package challenge.foro.hub.api.Proyecto.foro.hub.domain.usuario;
 
-import challenge.foro.hub.api.Proyecto.foro.hub.domain.perfil.Perfil;
+import challenge.foro.hub.api.Proyecto.foro.hub.domain.usuario.DTO.DatosNuevoUsuario;
 import challenge.foro.hub.api.Proyecto.foro.hub.domain.usuario.DTO.DatosUsuario;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Table(name = "usuarios")
@@ -16,7 +20,8 @@ import java.util.List;
 @AllArgsConstructor
 @Getter
 @EqualsAndHashCode(of = "id")
-public class Usuario {
+//Implementa interfaz UserDetails para security
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -30,6 +35,50 @@ public class Usuario {
         nombre = autor.nombre();
         email = autor.email();
         usuario = autor.usuario();
-        contrasena = autor.contrasena();
     }
+
+    public Usuario(DatosNuevoUsuario nuevoUsuario) {
+        nombre = nuevoUsuario.nombre();
+        email = nuevoUsuario.email();
+        usuario = nuevoUsuario.usuario();
+        contrasena = nuevoUsuario.contrasena();
+    }
+
+    //Configuracion Security
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return contrasena;
+    }
+
+    @Override
+    public String getUsername() {
+        return usuario;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+
 }
